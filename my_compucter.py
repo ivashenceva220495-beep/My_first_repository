e8bf4deab2377067bc15f7d6978054fb2168509c
annasandybaeva <ivashenceva220495@gmail.com>
2026-04-03 22:15:19 +0300

    данные о компьютере

---
+++ my_compucter.py
@@ -0,0 +1,11 @@
+import platform
+
+print("Operating System:", platform.system())
+print("Node Name:", platform.node())
+print("OS Release:", platform.release())
+print("OS Version:", platform.version())
+print("Machine:", platform.machine())
+print("Processor:", platform.processor())
+print("Architecture:", platform.architecture())
+print("Python Version:", platform.python_version())
+print("Python Compiler:", platform.python_compiler())
